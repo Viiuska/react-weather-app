ce333585f1ff5a6104d9891f8bdfa40f5caadca9
Viiuska <87257685+Viiuska@users.noreply.github.com>
2023-09-14 14:22:12 +0300

    Building city search and weather components

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions={
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fea7356057msheed65cb6d92da75p1b282ajsn66a9fbee3b9e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL='https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+
